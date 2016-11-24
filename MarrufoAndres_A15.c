7345521ba6af9a8ef4100c29a525715f5da3de7d
MarrufoAndres <andres.f.enix@hotmail.com>
2016-11-24 14:03:28 -0600

    tarea 2

---
+++ MarrufoAndres_A15.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+void ini_array(){
+
+
+}
+void mod_array(){
+
+}
+void sup_array(){
+
+}
+
+void search_array(){
+
+}
+void listar_agenda(){
+  
+}
+
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
