b0e33934fcd205338ce59c26587fb5cd433e7d0d
MarrufoAndres <andres.f.enix@hotmail.com>
2016-11-28 17:21:27 -0600

    hola

--- MarrufoAndres_A15.c
+++ MarrufoAndres_A15.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-
-typedef struct contac{
-  char nombre[100];
-  char apellidos[100];
-  char email[100];
-  char cel_num[10];
-
-} contacto;
+#include <stdlib.h>
+#include "contacto.h"
+#include "agenda.h"
 
 int main(int argc, char const *argv[]) {
   contacto agenda[20];
-  for (size_t i = 0; i < 20; i++) {
-    scanf("%s", agenda[i].nombre );
-  }
-
-  for (size_t i = 0; i < 20; i++) {
-    printf("%s\n", agenda[i].nombre);
-  }
+  system("clear");
 
   return 0;
 }
